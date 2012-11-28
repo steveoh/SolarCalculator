2b97f5174a4800c182cb04581cc7cdd158219ca4
steveoh <sgourley@utah.gov>
2012-11-28 15:14:35 -0700

    added license

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,23 @@
-﻿using System.Reflection;
+﻿#region License
+// 
+// Copyright (C) 2012 AGRC
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+// and associated documentation files (the "Software"), to deal in the Software without restriction, 
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+// subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial 
+// portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+#endregion
+using System.Reflection;
 using System.Runtime.InteropServices;
 using ESRI.ArcGIS.SOESupport;
 
@@ -24,8 +43,8 @@ using ESRI.ArcGIS.SOESupport;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("21774da8-e926-4eb5-97e6-b77194eb73aa")]
-[assembly: AssemblyVersion("0.1.155.1128")]
-[assembly: AssemblyFileVersion("0.1.155.1128")]
+[assembly: AssemblyVersion("0.1.156.1128")]
+[assembly: AssemblyFileVersion("0.1.156.1128")]
 [assembly: AddInPackage("SolarCalculator", "f06c4e70-96b0-4d72-a4d5-71ea0d314652",
     Author = "mapserv",
     Company = "AGRC",
